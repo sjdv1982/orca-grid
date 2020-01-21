981426b15452a5c64fe792a7b611fe2456f765f5
Sjoerd de Vries <sjdv1982@gmail.com>
2020-01-22 00:28:25 +0100

    excluded volume works

--- gen-arr.py
+++ gen-arr.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.signal
 from scipy.spatial import cKDTree as KDTree
 import itertools
 
@@ -10,6 +11,7 @@ mins = coor.min(axis=0)
 maxes = coor.max(axis=0)
 bounds=np.max([-mins, maxes],axis=0)
 bounds += clash_threshold
+bounds += extrusion
 bounds -= 0.5 * voxelsize
 
 superdivision_level = 0
@@ -20,24 +22,17 @@ while 1:
     superdivision_level += 1
 
 superdivision_level = 1
-print("SUPER!", superdivision_level)
+#print("SUPER!", superdivision_level)
 
 center = (bounds/voxelsize+0.999).astype(int)
 dims = center * 2
 first_voxel = -(center - 0.5) * voxelsize
-arr = np.zeros(shape=dims, dtype=np.float32)
+arr = np.zeros(shape=dims, dtype=bool)
 
 supervoxelsize = 2**superdivision_level * voxelsize
 supercenter = ((bounds+voxelsize)/supervoxelsize+0.999).astype(int)
 superdims = supercenter * 2
 first_supervoxel = -(supercenter - 0.5) * supervoxelsize
-"""
-for n in range(3):
-    d = first_voxel[n] - first_supervoxel[n]
-    if d % voxelsize:
-        first_supervoxel[n] += d % voxelsize 
-"""
-print(dims, superdims, first_voxel, first_supervoxel)
 
 def cartesian_coord(*arrays):
     # https://stackoverflow.com/a/25463061
@@ -90,7 +85,7 @@ def classify(points, curr_voxelsize):
     return ambiguous_points, definite_points
 
 def set_grid_blocks(blocks, block_superdivision):
-    print("SET GRID BLOCKS", blocks.shape, block_superdivision)
+    #print("SET GRID BLOCKS", blocks.shape, block_superdivision)
     points = blocks
     for n in range(block_superdivision, 0, -1):
         curr_voxelsize = voxelsize * 2**n
@@ -119,7 +114,7 @@ for n in range(superdivision_level, -1, -1):
         curr_points, curr_voxelsize
     )
     ldef = None if definite_points is None else len(definite_points)
-    print("SUPERDIV", n, curr_voxelsize, len(curr_points), len(ambiguous_points), ldef)
+    #print("SUPERDIV", n, curr_voxelsize, len(curr_points), len(ambiguous_points), ldef)
     if definite_points is not None and len(definite_points):
         set_grid_blocks(definite_points, n)
     if n > 0:
@@ -135,7 +130,7 @@ while len(ambiguous_subpoints):
     curr_voxelsize = 0.5**subdivision_level * voxelsize
     curr_subpoints = subdivide(ambiguous_subpoints, curr_voxelsize)
     curr_subpoint_index = np.repeat(ambiguous_subpoint_index, 8)
-    print("SUBDIV", subdivision_level,curr_voxelsize, len(curr_subpoints), len(np.unique(curr_subpoint_index)))
+    #print("SUBDIV", subdivision_level,curr_voxelsize, len(curr_subpoints), len(np.unique(curr_subpoint_index)))
     matches = coor_tree.query_ball_point(
         curr_subpoints, 
         clash_threshold, 
@@ -143,9 +138,9 @@ while len(ambiguous_subpoints):
     )
     match_mask = matches.astype(bool)
     impossible_index = np.unique(curr_subpoint_index[~match_mask])
-    print("ELIMINATE", len(impossible_index))
+    #print("ELIMINATE", len(impossible_index))
     subpoint_mask = np.isin(curr_subpoint_index, impossible_index, invert=True)
-    print("FILTER", subpoint_mask.sum(), len(curr_subpoints))
+    #print("FILTER", subpoint_mask.sum(), len(curr_subpoints))
     keep_mask = subpoint_mask & match_mask
     assert keep_mask.sum() == subpoint_mask.sum()
 
@@ -155,7 +150,7 @@ while len(ambiguous_subpoints):
         break
 
     delta = curr_voxelsize * half_sqrt_three
-    print("DELTA", delta)
+    #print("DELTA", delta)
     if delta > clash_threshold:
         ambiguous_subpoints = curr_subpoints2
         ambiguous_subpoint_index = curr_subpoint_index2
@@ -177,7 +172,54 @@ while len(ambiguous_subpoints):
     
     ambiguous_subpoints = curr_subpoints2[keep_mask]
     ambiguous_subpoint_index = curr_subpoint_index2[keep_mask]
-    print("SELECT", len(definite_points), len(ambiguous_subpoints), len(curr_subpoints2))
-    print()
+    #print("SELECT", len(definite_points), len(ambiguous_subpoints), len(curr_subpoints2))
+    #print()
 
-result = arr
+kdim0 = np.ceil(extrusion/voxelsize)
+krange = np.arange(-kdim0,kdim0+1)
+kdim = len(krange)
+kernel = cartesian_coord(krange, krange, krange) * voxelsize
+kernel = (kernel*kernel).sum(axis=1).reshape(kdim, kdim, kdim)
+kernel = (kernel<(extrusion**2))
+
+def fill_outer_shell(a):
+    a[[0,-1],:,:] = 1
+    a[:, [0,-1], :] = 1
+    a[:, :, [0,-1]] = 1
+
+
+small = 0.01
+arr_extrude = scipy.signal.convolve(arr.astype(np.float32), kernel, "same")
+arr_extrude = (arr_extrude > small)
+solvent = np.zeros(arr.shape, bool)
+fill_outer_shell(solvent)
+inv_arr_extrude = (~arr_extrude) | solvent
+solvent_size = solvent.sum()
+neighbor_kernel = np.ones((3,3,3), np.float32)
+print("conv")
+while 1:
+    solvent = solvent.astype(np.float32)
+    solvent = scipy.signal.convolve(solvent, neighbor_kernel, "same")
+    solvent = (solvent > small)
+    solvent = (solvent & inv_arr_extrude)
+    new_solvent_size = solvent.sum()
+    print(solvent_size, new_solvent_size)
+    if new_solvent_size == solvent_size:
+        break
+    solvent_size = new_solvent_size
+solvent_extrude = scipy.signal.convolve(solvent.astype(np.float32), kernel, "same")
+solvent_extrude = scipy.signal.convolve(solvent_extrude, neighbor_kernel, "same")
+solvent_extrude = (solvent_extrude > small)
+#result = (arr | (~solvent_extrude)).astype(np.float32)
+result = (((~solvent_extrude) & arr_extrude) | arr)
+print(result.shape)
+
+while 1:
+    shell = np.zeros(result.shape, bool)
+    fill_outer_shell(shell)
+    if (result & shell).sum():
+        break
+    result = result[1:-1,1:-1,1:-1]
+
+print(result.shape)
+result = result.astype(np.float32)
